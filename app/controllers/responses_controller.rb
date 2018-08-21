c7a2c889549bae17ce2ff47d2ef32c0ded8286c8
Matt West <hello@mattwest.io>
2018-08-21 13:42:52 +0100

    Updated to responses create method.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -10,19 +10,16 @@ class ResponsesController < ApplicationController
     article_id = message['MailboxHash']
 
     # Find the article
-    if article = Article.find(article_id)
-      # Create a new response
-      article.responses.create(
-        name: message['FromName'],
-        email: message['From'],
-        body: message['TextBody'] # Could also use message['HtmlBody']
-      )
+    article = Article.find(article_id)
 
-      # Return a 200 code so Postmark know’s the webhook was processed
-      render plain: 'Response Saved', status: 200
-    else
-      # Return a 404 error as the article wasn’t found
-      raise ActionController::RoutingError.new('Not Found')
-    end
+    # Create a new response
+    article.responses.create(
+      name: message['FromName'],
+      email: message['From'],
+      body: message['TextBody'] # Could also use message['HtmlBody']
+    )
+
+    # Return a 200 code so Postmark know’s the webhook was processed
+    render plain: 'Response Saved', status: 200
   end
 end
